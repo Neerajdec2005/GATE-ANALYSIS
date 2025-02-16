810a46ea3cdfd43c883d865f8984bc11de39584e
Neeraj T <165806591+Neerajdec2005@users.noreply.github.com>
2025-02-16 22:41:46 +0530

    Update App.tsx

--- frontend2/src/App.tsx
+++ frontend2/src/App.tsx
@@ -31,10 +31,10 @@ const App: React.FC = () => {
   return (
     <div className="container">
       <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm px-4 py-3 mb-4 rounded">
-  <a href="https://reaxt.vercel.app/" className="text-decoration-none"><span className="navbar-brand fw-bold">Exam Tracker</span></a>
+  <a href="https://gate-analysis.vercel.app/" className="text-decoration-none"><span className="navbar-brand fw-bold">Exam Tracker</span></a>
   <div className="d-flex gap-3"> <br />
     <a href="#" className="text-dark text-decoration-none">Explore</a>
-    <a href="#" className="text-dark text-decoration-none">Analysis</a>
+    <a href="https://gate-analyze.vercel.app/" className="text-dark text-decoration-none">Analysis</a>
     <a href="#" className="text-dark text-decoration-none">Feedback</a>
   </div>
 </nav>

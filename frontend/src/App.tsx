44d6c5d3fe93390afdc86af6759097c05e959557
Neeraj T <165806591+Neerajdec2005@users.noreply.github.com>
2025-02-19 14:31:33 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -136,7 +136,7 @@ function App() {
         <div className="flex items-center gap-6">
           <a href="https://department-analysis-fegrvrfgv34325.vercel.app/" className="text-gray-800 hover:text-indigo-600 no-underline">Department</a>
           <a href="https://student-analyze-bfdfgr3245.vercel.app/" className="text-gray-800 hover:text-indigo-600 no-underline">Student</a>
-          <a href="https://subject-wise-analysis-fdsgern.vercel.app/" className="text-gray-800 hover:text-indigo-600 no-underline">Topic-Wise</a>
+          <a href="#" className="text-gray-800 hover:text-indigo-600 no-underline">Topic-Wise</a>
           <a href="https://gate-analysis-kfcf.vercel.app/" className="text-gray-800 hover:text-indigo-600 no-underline">Upload</a>
         </div>
       </nav>
